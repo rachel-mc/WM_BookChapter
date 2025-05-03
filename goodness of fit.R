8d43dc6d35041725ff9f276e4bad2f7128f01e73
Rachel McInerney <rachel.mcinerney.2021@mumail.ie>
2025-05-04 00:04:31 +0100

    R^2 illustrative examples

--- goodness of fit.R
+++ goodness of fit.R
@@ -1,7 +1,16 @@
-## Chi-squared goodness-of-fit test
-chisq.test()
 
-## rootograms
+## Load the libraries
+
+library(ggplot2)
 library(gratia)
-fg <- rootogram(f5)
-draw(rg)
+library(MuMIN)
+library(performance)
+
+
+# R^2 ---------------------------------------------------------------------
+
+r2(f3)
+r2(f4) # Poisson model has good fit but is inadequate
+
+Rsq(f17) 
+Rsq(f18) # GPO and DPO have moderate goodness-of-fit but are adequate
