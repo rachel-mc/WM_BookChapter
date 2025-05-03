e255a806399ccc897be94a2ab0031ce30618832e
Rachel McInerney <rachel.mcinerney.2021@mumail.ie>
2025-05-04 00:08:11 +0100

    Information Criteria via MuMIn and Likelihood Ratio Tests

--- model selection.R
+++ model selection.R
@@ -2,11 +2,51 @@
 ## Load the libraries
 library(MuMIn)
 
-### Likelihood Ratio Tests for Nested models
-drop1(f4) # removing year improves the AIC??
 
-### Information Criteria
+# Information Criteria ----------------------------------------------------
+
+
+BICc <- function (obj) {
+  bic <- BIC(obj)
+  n <- nrow(obj$data)
+  bicc <- bic + (log(n) * (np + 1) * np)/(n - np - 1)
+  return(bicc)
+}
+
 # lme4
-AIC(f2, f3)
+AIC()
+BIC()
+AICc()
+BICc()
 
-### Cross Validation
+## glmmTMB
+
+## gamlss
+
+model_selection <- model.sel(f17, f18) # use pre-determined adequate models only
+model_selection
+candidate_models <- get.models(model_selection, subset = TRUE)
+candidate_models
+top_ranking_model <- candidate_models[[1]]
+top_ranking_model
+
+
+# Likelihood Ratio Tests --------------------------------------------------
+
+
+## Compare the nested zero-inflated models with their non-zero-inflated counterparts
+LR.test(f12, f13)
+LR.test(f14, f16)
+
+## Variable Selection
+drop1(f2, test = "Chisq") # dropping vegetation is slightly more favoured
+drop1(f3, test = "Chisq") 
+drop1(f4, test = "Chisq") 
+drop1(f5, test = "Chisq") 
+drop1(f6, test = "Chisq") 
+drop1(f8, test = "Chisq") 
+drop1(f9, test = "Chisq") 
+
+
+# Cross Validation --------------------------------------------------------
+
