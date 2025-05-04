c47fe872bdfde642be9b9f4a370de8f6b6067f57
Rachel McInerney <rachel.mcinerney.2021@mumail.ie>
2025-05-04 17:38:23 +0100

    Conditional/marginal/pseudo-R^2 for models that converged

--- goodness of fit.R
+++ goodness of fit.R
@@ -1,16 +1,37 @@
 
 ## Load the libraries
 
-library(ggplot2)
-library(gratia)
-library(MuMIN)
+library(gamlss)
 library(performance)
 
 
-# R^2 ---------------------------------------------------------------------
+# Pseudo/Conditional/Marginal R^2 ---------------------------------------------------------------------
 
-r2(f3)
-r2(f4) # Poisson model has good fit but is inadequate
+## lme4
 
-Rsq(f17) 
-Rsq(f18) # GPO and DPO have moderate goodness-of-fit but are adequate
+r2(f3) # Negative Binomial type 2 model has a moderate fit to the data
+
+## glmmTMB
+
+r2(f4) # Poisson model has an excellent fit to the data but is inadequate
+r2(f5) # Zero-inflated Poisson model has an ok fit to the data
+r2(f6) # Hurdle Poisson model has a very high variance explained by both the fixed and random effects
+r2(f7) # Negative Binomial type 2 model has a decent fit to the data
+r2(f8) # Zero-inflated Negative Binomial type 2 model has an excellent fit to the data
+r2(f9) # Generalised Poisson model has a good fit to the data
+r2(f10) ## Slow and nonsensical
+r2(f11) # Negative Binomial type 1 model has a good fit to the data
+
+## gamlss
+
+# Value is interpreted as the proportion of deviance explained by the model
+# How well the mean structure explains the response
+
+Rsq(f12) # Poisson model has an excellent fit to the data but is inadequate
+Rsq(f13) # Zero-inflated Poisson model has an even better fit to the data but is inadequate
+Rsq(f14) # Negative Binomial type 2 model has a poor fit to the data
+Rsq(f16) # Negative Binomial type 1 model has a poor fit to the data
+Rsq(f17) # Generalised Poisson model is adequate but has a mediocre fit to the data
+Rsq(f18) # Poisson-Inverse Gaussian model is adequate but has a mediocre fit to the data
+Rsq(f20) # Model with a variance structure has a poor fit to the data
+Rsq(f21) # Model with a cubic natural spline has has a poor fit to the data
