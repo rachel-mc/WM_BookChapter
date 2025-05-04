eb12ea1f2a4d31537a55ada77d7f4583113d5a7a
Rachel McInerney <rachel.mcinerney.2021@mumail.ie>
2025-05-04 16:56:48 +0100

    Removed model that did not converge

--- model adequacy.R
+++ model adequacy.R
@@ -4,22 +4,24 @@
 library(gamlss)
 library(hnp)
 
-### Assessing model adequacy using Half Normal Plots 
+
+# Assessing model adequacy using Half Normal Plots ------------------------
+
 
 ## lme4
 
 set.seed(2025) # for reproducability
 
+## Poisson model did not converge - for illustrative purposes only
 hnp(f2,
     how.many.out = TRUE,
     paint = T)
 
-### Worm plots for 'gamlss' models
+### Worm plots for converged 'gamlss' models
 
 wp(f12)
 wp(f13)
 wp(f14)
-wp(f15)
 wp(f16)
 wp(f17)
 wp(f18)
