7dfcc2745664299520439fa71099f9b529886e2c
Rachel McInerney <rachel.mcinerney.2021@mumail.ie>
2025-05-04 17:54:43 +0100

    Model selection using different types of information criteria. Need to update the glmmTMB models so that only adequate ones are compared

--- model selection.R
+++ model selection.R
@@ -1,10 +1,26 @@
 
 ## Load the libraries
+
+library(gamlss)
 library(MuMIn)
 
 
 # Information Criteria ----------------------------------------------------
 
+### lme4: Only the Negative Binomial model converged so there is no model comparison in this case
+
+
+### glmmTMB
+
+model.sel(f4, f5, f6, f7, f8, f9, f10, f11, # use pre-determined ADEQUATE models only
+          rank = "AIC") 
+
+model.sel(f4, f5, f6, f7, f8, f9, f10, f11) 
+
+model.sel(f4, f5, f6, f7, f8, f9, f10, f11,
+          rank = "BIC") 
+
+## BICc function
 
 BICc <- function (obj) {
   bic <- BIC(obj)
@@ -13,17 +29,27 @@ BICc <- function (obj) {
   return(bicc)
 }
 
-# lme4
-AIC()
-BIC()
-AICc()
-BICc()
+model.sel(f4, f5, f6, f7, f8, f9, f10, f11,
+          rank = "BICc") ## BICc function
 
-## glmmTMB
+## In all cases, the Negative Binomial type 2 model is preferred
 
-## gamlss
+### gamlss 
 
-model_selection <- model.sel(f17, f18) # use pre-determined adequate models only
+# BICc is not calculable for gamlss models - mathematical error
+
+model.sel(f17, f18, # use pre-determined adequate models only
+          rank = "AIC")
+
+model.sel(f17, f18)
+
+model.sel(f17, f18,
+          rank = "BIC")
+
+## The Generalised Poisson model is preferred by AIC and AICc but the Poisson Inverse Gaussian model
+## is preferred by BIC
+
+model_selection <- model.sel(f17, f18)
 model_selection
 candidate_models <- get.models(model_selection, subset = TRUE)
 candidate_models
@@ -34,11 +60,15 @@ top_ranking_model
 # Likelihood Ratio Tests --------------------------------------------------
 
 
-## Compare the nested zero-inflated models with their non-zero-inflated counterparts
-LR.test(f12, f13)
-LR.test(f14, f16)
+## Comparing the (nested) zero-inflated Poisson model in gamlss with its non-zero-inflated counterpart
+
+LR.test(f12, f13) 
+# p-value = 0 so we reject the null hypothesis that the simpler model is better
+# We can conclude that the Zero-inflated Poisson model is preferred over the Poisson model 
+# However, both are inadequate for this data
 
 ## Variable Selection
+
 drop1(f2, test = "Chisq") # dropping vegetation is slightly more favoured
 drop1(f3, test = "Chisq") 
 drop1(f4, test = "Chisq") 
