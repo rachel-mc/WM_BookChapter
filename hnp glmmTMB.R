5cd8fe69aac06c96a03db214e5e52e0ea2079b72
Rachel McInerney <rachel.mcinerney.2021@mumail.ie>
2025-05-04 15:43:20 +0100

    Updated model formula. Included the zero-inflated term. Added arguments to the hnp() function

--- hnp glmmTMB.R
+++ hnp glmmTMB.R
@@ -4,14 +4,15 @@
 library(glmmTMB)
 library(hnp)
 
-## Helper function for glmmTMB
+## Helper function for glmmTMB models
 
 hnp_glmmTMB <- function(model, 
                         ...) {
   
   # Extract components from the model
-  fam <- eval(model$call$family)
+  fam <- model$call$family
   mod_data <- eval(model$call$data)
+  zi <- model$call$ziformula
   
   # Diagnostic function
   dfun <- function(obj) residuals(obj, type = "pearson")
@@ -20,19 +21,25 @@ hnp_glmmTMB <- function(model,
   sfun <- function(n, obj) simulate(obj, nsim = n)[[1]]
   
   # Fitting function
-  ffun <- function(response) {
-    mod_data$response <- response
-    fit <- try(glmmTMB(response ~ ., 
+  ffun <- function(resp) {
+    mod_data$new_response <- resp
+    fit <- try(glmmTMB(new_response ~ year + scale(d1aug) + scale(longitude)
+                       + scale(temp) + scale(sal) + scale(depth) + as.numeric(vegetation) 
+                       + scale(moam) + (1 | station), 
                        family = fam,
-                       data = mod_data),
+                       data = mod_data,
+                       ziformula = zi),
                silent = TRUE)
     
     while (inherits(fit, "try-error")) {
       response2 <- sfun(1, model)
       mod_data$response2 <- response2
-      fit <- try(glmmTMB(response2 ~ ., 
+      fit <- try(glmmTMB(response2 ~ year + scale(d1aug) + scale(longitude)
+                         + scale(temp) + scale(sal) + scale(depth) + as.numeric(vegetation) 
+                         + scale(moam) + (1 | station), 
                          family = fam,
-                         data = mod_data),
+                         data = mod_data,
+                         ziformula = zi),
                  silent = TRUE)
     }
     return(fit)
@@ -51,10 +58,50 @@ hnp_glmmTMB <- function(model,
 
 set.seed(2025)
 
-hnp_glmmTMB(f4)
-hnp_glmmTMB(f5)
-hnp_glmmTMB(f6)
-hnp_glmmTMB(f7)
-hnp_glmmTMB(f8)
-hnp_glmmTMB(f9)
-hnp_glmmTMB(f11)
+hnp_glmmTMB(f4,
+            how.many.out = T,
+            paint = T,
+            sim = 19, 
+            conf = 1)
+
+hnp_glmmTMB(f5,
+            how.many.out = T,
+            paint = T,
+            sim = 19, 
+            conf = 1)
+
+hnp_glmmTMB(f6,
+            how.many.out = T,
+            paint = T,
+            sim = 19, 
+            conf = 1)
+
+hnp_glmmTMB(f7,
+            how.many.out = T,
+            paint = T,
+            sim = 19, 
+            conf = 1)
+
+hnp_glmmTMB(f8,
+            how.many.out = T,
+            paint = T,
+            sim = 19, 
+            conf = 1)
+
+hnp_glmmTMB(f9,
+            how.many.out = T,
+            paint = T,
+            sim = 19, 
+            conf = 1)
+
+hnp_glmmTMB(f10,
+            how.many.out = T,
+            paint = T,
+            sim = 19, 
+            conf = 1)
+
+hnp_glmmTMB(f11,
+            how.many.out = T,
+            paint = T,
+            sim = 19, 
+            conf = 1)
