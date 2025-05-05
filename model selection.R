3ea6b10aa4a858f7fae0a2b82260b571add8a235
Rachel McInerney <rachel.mcinerney.2021@mumail.ie>
2025-05-05 02:28:55 +0100

    Variable selection (LRTs) and cross validation

--- model selection.R
+++ model selection.R
@@ -7,6 +7,7 @@ library(MuMIn)
 
 # Information Criteria ----------------------------------------------------
 
+
 ### lme4: Only the Negative Binomial model converged so there is no model comparison in this case
 
 
@@ -60,23 +61,60 @@ top_ranking_model
 # Likelihood Ratio Tests --------------------------------------------------
 
 
-## Comparing the (nested) zero-inflated Poisson model in gamlss with its non-zero-inflated counterpart
+## Variable Selection - adequate models only
 
-LR.test(f12, f13) 
-# p-value = 0 so we reject the null hypothesis that the simpler model is better
-# We can conclude that the Zero-inflated Poisson model is preferred over the Poisson model 
-# However, both are inadequate for this data
+drop1(f3, test = "Chisq") # dropping depth is slightly more favoured
 
-## Variable Selection
+drop1(f4, test = "Chisq") # dropping vegetation is slightly more favoured
+drop1(f5, test = "Chisq") # model is preferred as is
+drop1(f6, test = "Chisq") # model is preferred as is
+drop1(f7, test = "Chisq") # dropping sal or depth is slightly more favoured
+drop1(f8, test = "Chisq") # dropping sal or depth is slightly more favoured
+drop1(f9, test = "Chisq") # dropping d1aug is slightly more favoured
+# drop1(f10, test = "Chisq") - ignored due to run time
+drop1(f11, test = "Chisq") # dropping d1aug is slightly more favoured
 
-drop1(f2, test = "Chisq") # dropping vegetation is slightly more favoured
-drop1(f3, test = "Chisq") 
-drop1(f4, test = "Chisq") 
-drop1(f5, test = "Chisq") 
-drop1(f6, test = "Chisq") 
-drop1(f8, test = "Chisq") 
-drop1(f9, test = "Chisq") 
+drop1(f17) # dropping d1aug, sal, depth, or vegetation is slightly more favoured
+drop1(f18) # dropping d1aug, sal, or depth is slightly more favoured
 
 
 # Cross Validation --------------------------------------------------------
 
+
+## Compare the predictive performance of the adequate gamlss models
+
+# Generalised Poisson 
+
+n <- nrow(na.omit(bass))
+pred <- numeric(n) 
+
+for (i in 1:n) {
+  train_data <- na.omit(bass)[-i,] 
+  test_data <- na.omit(bass)[i, , drop = FALSE]
+  
+  mod <- gamlss(mosa ~ year + scale(d1aug) + scale(longitude) + scale(temp) + scale(sal) + scale(depth) + as.numeric(vegetation) + scale(moam) + random(station),
+                family = "GPO",
+                data = train_data)  
+  
+  pred[i] <- predict(mod, newdata = test_data, type = "response")
+}
+
+mean((bass$mosa - pred)^2) # Mean-squared error = 1.765303e+27
+
+## Poisson-Inverse Gaussian
+
+n <- nrow(na.omit(bass))
+pred <- numeric(n) 
+
+for (i in 1:n) {
+  train_data <- na.omit(bass)[-i,] 
+  test_data <- na.omit(bass)[i, , drop = FALSE]
+  
+  mod <- gamlss(mosa ~ year + scale(d1aug) + scale(longitude) + scale(temp) + scale(sal) + scale(depth) + as.numeric(vegetation) + scale(moam) + random(station),
+                family = "PIG",
+                data = train_data)  
+  
+  pred[i] <- predict(mod, newdata = test_data, type = "response")
+}
+
+mean((bass$mosa - pred)^2) # Mean-squared error = 782023.8
