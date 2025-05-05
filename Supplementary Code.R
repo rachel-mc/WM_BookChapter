1a3ba7bdbc137023057198037957df06f5e8cf72
Rachel McInerney <rachel.mcinerney.2021@mumail.ie>
2025-05-06 00:42:44 +0100

    Candidate reduced model that is preferred to full counterpart

--- Supplementary Code.R
+++ Supplementary Code.R
@@ -13,6 +13,8 @@ library(tidyverse)
 
 # Code to filter the raw data to obtain the 'clean' subset used 
 
+### Incomplete
+
 fn <- "~/Desktop/Data/YOY_Striped_Bass_2013_2023.xlsx"
 
 bass_raw <- read_excel(fn, 
@@ -63,11 +65,22 @@ summary(fit4)
 anova(f2, fit4, test = "Chisq") 
 # the model with the random effect is preferred
 
-# Half Normal plot of the Poisson GLMM
+# Half Normal plot of the Poisson GLMM (that did not converge)
 hnp(f2,
     how.many.out = T,
     paint = T) 
 # pattern indicative of overdispersion - variance >> mean
+
+
+# Is the zero-inflated term necessary? -------------------------------------------
+
+
+## Comparing the (nested) zero-inflated Poisson gamlss model with its non-zero-inflated counterpart
+
+LR.test(f12, f13) 
+# p-value = 0 so we reject the null hypothesis that the simpler model is better
+# We can conclude that the Zero-inflated Poisson model is preferred over the Poisson model 
+# However, both are inadequate for this dataset.
 
 
 # Assess the effect of Tide on Mosa -----------------------------------------------
@@ -80,6 +93,7 @@ fit5 <- gamlss(mosa ~ year + scale(d1aug) + scale(longitude) + scale(tide) + sca
               data = na.omit(bass)) # already reduced dataset
 
 summary(fit5)
+## not statistically supported by the Wald test
 
 
 # Variable Significance ---------------------------------------------------
@@ -87,13 +101,28 @@ summary(fit5)
 
 ## Covariates that have no statistical support could be removed from the data to further maximise the available information
 
+## Consider the top-ranking adequate GPO model
+
+summary(f17)
+
+## Refit the model with d1aug, sal, depth, and vegetation omitted
+
+fit6 <- gamlss(mosa ~ year + scale(longitude) + scale(temp) + scale(moam) + random(station),
+              family = GPO,
+              data = na.omit(bass))
+
+summary(fit6)
+
+model.sel(f17, fit6)
+model.sel(f17, fit6, rank = "BIC")
+## The reduced model is preferred by both IC
 
 
 # Temporal Autocorrelation ------------------------------------------------
 
 
+check_autocorrelation(f17)
 check_autocorrelation(f18)
-
 
 
 # Standard errors ---------------------------------------------------------
